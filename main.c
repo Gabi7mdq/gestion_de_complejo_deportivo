dd5d93c686a98c707c677a71ca6fef6d767eedaf
Cepe_S <santicepeda03@gmail.com>
2022-05-31 09:56:20 -0300

    estructuras agregadas

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 #include <string.h>
 
 typedef struct {
-    
-}
+    // f: futbol, b: basket, t: tenis, p: padel, w: bowling
+    char tipo;
+    // por hora
+    int precio;
+    // 2: si está rota, 1: si esta ocupada, 0: si no esta libre
+    int estado;
+    int capacidad;
+} cancha;
+
+typedef struct {
+    char apellido[20];
+    // id único
+    int dni;
+    int telefono;
+} cliente;
+
